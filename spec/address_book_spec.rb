132d4445b3b6c09cf5a85d4a17bbbf8c185af6e5
ian bevel & chris sefkin <student@epicodus.com>
2015-04-30 09:41:50 -0700

    add a spec to test last name, passes

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -8,9 +8,16 @@ describe(Contact) do
   # end
 
   describe('#first_name') do
-    it('will allow for entry of contact name') do
+    it('will allow for entry of contact first name') do
       test_name = Contact.new({:first_name => "Chris", :last_name => "Sefkin", :birthday => "08/08/83"})
       expect(test_name.first_name()).to(eq("Chris"))
+    end
+  end
+
+  describe('#last_name') do
+    it('will allow for entry of contact last name') do
+      test_name = Contact.new({:first_name => "Chris", :last_name => "Sefkin", :birthday => "08/08/83"})
+      expect(test_name.last_name()).to(eq("Sefkin"))
     end
   end
 
